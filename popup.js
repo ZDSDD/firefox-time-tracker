895c93905c8bbd0eda7ed046836c166a5ff4f5fb
ZDSDD <sebasadowy@gmail.com>
2025-12-11 22:27:12 +0100

    one commit

--- popup.js
+++ popup.js
@@ -157,8 +157,7 @@ class PopupUI {
 
         if (this.blockCheck.checked) {
             blocking[this.currentSite] = true;
-        }
-        {
+        } else {
             delete blocking[this.currentSite];
         }
 
